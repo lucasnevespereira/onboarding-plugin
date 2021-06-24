27b5483833f11200d7dafe5fc601c7f3e1c9d532
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-06-24 17:30:16 +0200

    created bot

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/lucasnevespereira/onboarding-plugin
+
+go 1.16
+
+require (
+	github.com/mattermost/mattermost-server/v5 v5.32.1
+	github.com/pkg/errors v0.9.1
+	github.com/robfig/cron v1.2.0 // indirect
+	github.com/stretchr/testify v1.7.0
+)
---
+++ server/plugin.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/plugin"
+)
+
+// Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
+type Plugin struct {
+	plugin.MattermostPlugin
+
+	// configurationLock synchronizes access to the configuration.
+	configurationLock sync.RWMutex
+
+	// configuration is the active plugin configuration. Consult getConfiguration and
+	// setConfiguration for usage.
+	configuration *configuration
+}
+
+// ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
+func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
+
+// OnActivate creates bot
+func (p *Plugin) OnActivate() error {
+	b := &model.Bot{
+		Username:    "catherinebot",
+		DisplayName: "catherinebot",
+		Description: "Catherine increases your daily workflow",
+	}
+
+	_, err := p.API.CreateBot(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// See https://developers.mattermost.com/extend/plugins/server/reference/
